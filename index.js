1cc941c2768ec5093ae0a416f09c9e5aaf1e641d
jrizza88 <jetorres88@gmail.com>
2019-03-07 19:59:30 -0500

    added all dependecies and dev dependecies and set up index file to listen for the port

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+
+const server = require('./server');
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () =>{
+    console.log(`Server is listening on port: ${port}`)
+});
